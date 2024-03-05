e2d7602d388356434cb0097b0ef1388e0fa5a1e6
SilviuIordache <silviu.iordache92@gmail.com>
2024-03-05 16:58:14 +0200

    image optimisations + gtag script integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { getServerSession } from 'next-auth';
-import ProductCard from './users/components/ProductCard';
 import { authOptions } from './api/auth/[...nextauth]/route';
+import Image from 'next/image';
+
+// import coffee from '@/public/images/coffee.png';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -8,7 +10,22 @@ export default async function Home() {
   return (
     <main>
       <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <ProductCard />
+
+      <div className="relative h-80">
+        {/* static image */}
+        {/* <Image src={coffee} alt="coffee" /> */}
+
+        {/* remote image*/}
+        <Image
+          src="https://bit.ly/react-cover"
+          alt="coffee"
+          fill
+          className="object-contain"
+          sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+          quality={75}
+          priority
+        />
+      </div>
     </main>
   );
 }
---
+++ app/scripts/MainPageScripts.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GoogleAnalyticsScript from './GoogleAnalyticsScript';
+
+const MainPageScripts = () => {
+  return (
+    <div>
+      <GoogleAnalyticsScript />
+    </div>
+  );
+};
+
+export default MainPageScripts;
